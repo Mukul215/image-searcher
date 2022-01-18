dc29563de7c9c3b4469555707ea3ade5464c417f
Mukul Patel <Mukul215@gmail.com>
2022-01-17 22:00:55 -0500

    added vercel json

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 export default class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 150);
+
+    this.imageRef.current.style.gridRowEnd = `span ${spans}`;
+  };
+
   render() {
     const { description, urls } = this.props.images;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
